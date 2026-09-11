3e0962a0100e4b93c07dc26a60c50c520fcef93e
Dillon Rinkwest <ST10439147@myemeris.edu.za>
2026-09-11 10:13:57 +0200

    Add BinaryTreeSearch.Classes and build sample tree
    Added using directive for BinaryTreeSearch.Classes. Constructed a sample binary tree of integers in Main by creating a root node and adding left/right children.

--- BinaryTreeSearch/Program.cs
+++ BinaryTreeSearch/Program.cs
@@ -1,7 +1,16 @@
-﻿public class Program
+﻿using BinaryTreeSearch.Classes;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        
+        TreeNode<int> root = new TreeNode<int>(10);
+        root.Left = new TreeNode<int>(5);
+        root.Right = new TreeNode<int>(15);
+        root.Left.Left = new TreeNode<int>(3);
+        root.Left.Right = new TreeNode<int>(7);
+        root.Right.Right = new TreeNode<int>(20);
+        root.Right.Left = new TreeNode<int>(12);
+        root.Left.Left.Left = new TreeNode<int>(1);
     }
 }
