319a997f5e721fc271a9d0b4fe3fb0ff15ddb345
Dillon Rinkwest <ST10439147@myemeris.edu.za>
2026-09-11 08:44:18 +0200

    Add generic TreeNode<T> class; clean up csproj ItemGroup
    Added TreeNode.cs with a generic TreeNode<T> in BinaryTreeSearch.Classes. Removed an unused <ItemGroup> referencing the Classes folder from BinaryTreeSearch.csproj.

---
+++ BinaryTreeSearch/Classes/TreeNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeSearch.Classes
+{
+    public class TreeNode<T> where T : IComparable<T>
+    {
+        public T Data { get; set; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+
+    }
+}
